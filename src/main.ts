0563b38829b7721093bcfdecdaf36d316986eb28
Brown-cello <flexwidflash@gmail.com>
2025-05-29 21:53:23 +0100

    blocke user and unblock user

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
+import { seedAdmin } from './seed-admin';
 
 config();  
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  if (process.env.RUN_SEED === 'true') {
+    await seedAdmin();
+  }
 
   await app.listen(process.env.PORT ?? 3000);
 }
